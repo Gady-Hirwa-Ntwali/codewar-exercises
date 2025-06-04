753c588c1fe97f3f59cb19c98d321de5235e82e8
Hirwa Ntwali Gady <hirwagad767@gmail.com>
2025-06-04 13:16:18 +0200

    is about to chech if empoy is on vacation

--- ok.js
+++ ok.js
@@ -1,8 +1,9 @@
-function checkForFactor (base, factor) {
-  if(  base % factor ===0){
-    return true
+function setAlarm(employed, vacation){
+  if(employed === true && vacation === false){
+    return true;
   }else{
-    return false
+    return false;
   }
+
 }
-console.log(checkForFactor(2, 2))
+console.log(setAlarm(true, true))
