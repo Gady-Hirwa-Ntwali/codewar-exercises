f82b7277136766024499ff2d6f0390f00c648dbf
Hirwa Ntwali Gady <hirwagad767@gmail.com>
2025-06-12 10:12:11 +0200

    use include for making string case insensitive

--- ok.js
+++ ok.js
@@ -1,1 +1,11 @@
-
+function validateHello(greetings) {
+  const changed = greetings.toLowerCase()
+  return changed.includes('hello') || 
+  changed.includes('ciao') || 
+  changed.includes('salut') || 
+  changed.includes('hallo') || 
+  changed.includes('hola') || 
+  changed.includes('ahoj') ||
+  changed.includes('czesc')
+}
+console.log(validateHello('tRes: lA HastA, CiAO'))
