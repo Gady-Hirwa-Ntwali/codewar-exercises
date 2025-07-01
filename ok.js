afe65e885ea52e9b0ce095d4a3b7ead8d9be7c97
Hirwa Ntwali Gady <hirwagad767@gmail.com>
2025-07-01 07:44:21 +0200

    return max and min numb in array

--- ok.js
+++ ok.js
@@ -1,5 +1,7 @@
-function addBinary(a,b) {
-  const sum = a+b
-  return sum.toString(2)
-
+function minMax(arr){
+ const max  = Math.max(...arr)
+ const min = Math.min(...arr)
+ const newArray = [min, max]
+ return  newArray
 }
+console.log(minMax([1, 2, 3, 4, 5]))
